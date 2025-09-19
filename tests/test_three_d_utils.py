8cc6e75294874672200ef6440415200bf7e78809
Giancarlo Pereira <giancarlo.pereira@columbia.edu>
2025-09-19 18:06:27 -0400

    for the few tests, make them behave with the new classless utils

--- tests/test_three_d_utils.py
+++ tests/test_three_d_utils.py
@@ -14,7 +14,7 @@ FLOAT_EPS_SQ = 1.0e-14
 class TestThreeDUtils(unittest.TestCase):
 
     def test_fit_line(self):
-        _, v = ThreeDUtils.fit_line([[0,0,1], [0,0,2], [0,0,3], [0,0,4]])
+        _, v = fit_line([[0,0,1], [0,0,2], [0,0,3], [0,0,4]])
         result = v
         expected = np.array([0,0,1]).reshape((1,3))
         self.assertAlmostEqual(np.linalg.norm(abs(result) - expected), 0.0,
@@ -31,7 +31,7 @@ class TestThreeDUtils(unittest.TestCase):
         for _ in range(1000):
             points.append( [random.uniform(-10,10), random.uniform(-10,10), 0] )
 
-        _, n = ThreeDUtils.fit_plane(points)
+        _, n = fit_plane(points)
         result = n
         expected = np.array([0,0,1]).reshape((1,3))
         self.assertAlmostEqual(np.linalg.norm(abs(result) - expected), 0.0,
@@ -43,7 +43,7 @@ class TestThreeDUtils(unittest.TestCase):
         line2 = [[0,1,0], [0,-1,0]]
         line3 = [[0,0,1], [0,0,-1]]
 
-        result = ThreeDUtils.intersect_lines([line1,line2,line3])
+        result = intersect_lines([line1,line2,line3])
         expected = np.array([0, 0, 0]).reshape((1,3))
         
         self.assertAlmostEqual(np.linalg.norm(result - expected), 0.0,
@@ -52,7 +52,7 @@ class TestThreeDUtils(unittest.TestCase):
     def test_plane_line_intersection(self):
         plane = [[0,0,0], [0,0,1]]
         line = [[0,0,3], [1,1,-1]]
-        result = ThreeDUtils.intersect_line_with_plane(line[0], line[1], plane[0], plane[1])
+        result = intersect_line_with_plane(line[0], line[1], plane[0], plane[1])
         expected = np.array([3,3, 0]).reshape((1,3))
 
         self.assertAlmostEqual(np.linalg.norm(result - expected), 0.0, 
@@ -61,7 +61,7 @@ class TestThreeDUtils(unittest.TestCase):
     def test_point_line_distance(self):
         point = [1,0,1]
         line = [[0,0,0], [0,0,1]]
-        result = ThreeDUtils.point_line_distance(point, line[0], line[1])
+        result = point_line_distance(point, line[0], line[1])
         expected = 1.0
 
         self.assertAlmostEqual(result, expected,
